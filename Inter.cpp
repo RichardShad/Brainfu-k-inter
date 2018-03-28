93985e6f29052815b01dfc07b485d7571f57a02e
Richard <george.dianov42@gmail.com>
2018-03-28 14:03:38 +0300

    A bug fixed. If .. if .. if changed to switch-case.

--- Inter.cpp
+++ Inter.cpp
@@ -10,34 +10,52 @@ void Inter(TokenList* code){
 	int*					end = ptr + 1000;
 	std::stack <TokenList*>			CycleStack;
 
-      	for(TokenList *iterator; iterator != nullptr; iterator = iterator -> next){
-		if(iterator -> value.get_value() == Token::Operation::BrOpen)
-			CycleStack.push(iterator);
-		
-		if(iterator -> value.get_value() == Token::Operation::BrClose){
-			if(*ptr != 0)
-				iterator = CycleStack.top();
-			CycleStack.pop();
+      	for(TokenList *iterator = code; iterator != nullptr; iterator = iterator -> next){
+		switch(iterator -> value.get_value()){
+			case Token::Operation::BrOpen: {
+				CycleStack.push(iterator);
+				break;
+			}
+			
+			case Token::Operation::BrClose: {
+					if(!CycleStack.empty()){
+						if(*ptr != 0)
+							iterator = CycleStack.top();
+							CycleStack.pop();
+					}
+					break;
+				}
+				
+			case Token::Operation::Right: {
+				if(++ptr >= end)
+					ptr = &Cells[0];
+				break;
+			}
+			
+			case Token::Operation::Left: {
+				if(--ptr < &Cells[0])
+					ptr = (end - 1);
+				break;
+			}
+			case Token::Operation::Plus: {
+				(*ptr)++;
+				break;
+			}
+
+			case Token::Operation::Minus: {
+				(*ptr)--;
+				break;
+			}
+			
+			case Token::Operation::Comma: {
+				*ptr = getchar();
+				break;
+			}
+			
+			case Token::Operation::Dot:{
+				putchar(*ptr);
+				break;
+			}
 		}
-		
-		if(iterator -> value.get_value() == Token::Operation::Right)
-			if(++ptr >= end)
-				ptr = &Cells[0];
-		
-		if(iterator -> value.get_value() == Token::Operation::Left)
-			if(--ptr < &Cells[0])
-				ptr = (end - 1);
-		
-		if(iterator -> value.get_value() == Token::Operation::Plus)
-			(*ptr)++;
-		
-		if(iterator -> value.get_value() == Token::Operation::Minus)
-			(*ptr)--;
-		
-		if(iterator -> value.get_value() == Token::Operation::Comma)
-			*ptr = getchar();
-		
-		if(iterator -> value.get_value() == Token::Operation::Dot)
-			putchar(*ptr);
 	}
 }
